71c5059a7e3c8591f235a89b5cb52674fbeff359
Alexis Tuchinda <50222834+AlexisTuchinda@users.noreply.github.com>
2020-07-09 19:14:27 -0700

    Card and ShowCard
    Worked on Card Component and ShowCard hook/function

--- sharespace/src/components/Cards/Card.js
+++ sharespace/src/components/Cards/Card.js
@@ -8,7 +8,7 @@ class Card extends React.Component {
         this.state = {
             title = props.title,
             artist = props.user,
-            image = props.url,
+            image = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmiro.medium.com%2Fmax%2F2550%2F1*6d5dw6dPhy4vBp2vRW6uzw.png&imgrefurl=https%3A%2F%2Ftowardsdatascience.com%2Fpandas-groupby-aggregate-transform-filter-c95ba3444bbb&tbnid=-_NIe97rudDVsM&vet=12ahUKEwicj_PFyMHqAhWJop4KHWPdDz0QMygBegUIARDRAQ..i&docid=wSkn_oMXRKAgYM&w=1275&h=685&q=pandas&ved=2ahUKEwicj_PFyMHqAhWJop4KHWPdDz0QMygBegUIARDRAQ",
             votes = 0
         };
     }
@@ -21,17 +21,19 @@ class Card extends React.Component {
 
     render() {
         return (
-            //display title and creator
-            <div>
-                <h1>{this.state.title}+" by "+{this.state.artist}</h1>
-            </div>
-            //displays art; using link
-            <div>
-                <li><img src = {this.state.image}/></li>
-            </div>
-            //displays votes, allows users to vote
-            <div>
-                <h3>{this.state.votes}+" votes"</h3>
+            <div> 
+                <div>
+                    <h1>{this.state.title}+" by "+{this.state.artist}</h1>
+                </div>
+                <div>
+                    <img src = {this.state.image}/>
+                </div>
+                <div>
+                    <h3>{this.state.votes}+" votes"</h3>
+                </div>
+                <div>
+                    
+                </div>
             </div>
         );
     }
---
+++ sharespace/src/components/showCards.js
@@ -0,0 +1,11 @@
+import React,{useState} from "react";
+
+function showCards(props) {
+    
+    const cards = props.cards;
+    const show = cards.map((card) => 
+    <ul>(card)</ul>
+    );
+
+    return <div>{this.show}</div>
+};
